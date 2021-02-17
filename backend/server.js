0081691ba6a1705482f73c9a76412aeb0223e63e
Jonatan Tadeo <tadeo.calles@outlook.com>
2021-02-17 14:12:07 -0500

    frontend added, also connection between user and exercise creation and mongodb. solved CORS issue when we do origin-header requests.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT||5000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../')));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
